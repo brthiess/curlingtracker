3ecb8ec698a186ace59105f38125f94f7c17efc7
Brad <brthiess@ualberta.ca>
2018-09-27 20:34:15 -0600

    add rss reader

---
+++ RSSCrawler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using System.Xml;
+
+namespace RSSCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://fooblog.com/feed";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                String subject = item.Title.Text;
+                String summary = item.Summary.Text;
+            }
+        }
+    }
+}
